e273468be66f2a9df63854a19fed64df2a6bd937
Onur Seckin Senoglu <onurseckinsenoglu@gmail.com>
2023-10-22 21:41:51 -0700

    next-auth init

--- app/api/content/route.tsx
+++ app/api/content/route.tsx
@@ -31,8 +31,10 @@ const posts = [
   },
 ];
 
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
 export function GET() {
+  const session = await getServerSession();
   return NextResponse.json(posts);
 }
